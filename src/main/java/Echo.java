77eb4c0ec323fa0ec521a5b69cb7f51601661538
elliotjohnwu <elliotjohnwu@gmail.com>
2026-01-29 15:00:31 +0800

    Added Mark as done into Echo class

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -24,12 +24,25 @@ public class Echo {
                 break;
 
             } else if (line.equalsIgnoreCase("list")) {
-                for (int i = 0; i < listsTracker; i++ ) {
-                    System.out.println(Integer.toString(i+1) + ". " + lists[i]);
+                for (int i = 1; i <= listsTracker; i++ ) {
+                    System.out.println(Integer.toString(i) + ". " + lists[i-1]);
                 }
                 System.out.println("\n" + Jellyfish.space);
-            } else {
-                    lists[listsTracker] = line;
+            } else if (line.contains(("mark"))) {
+                int markedPosition;
+                String text = line.trim();
+                if (line.contains("unmark")) {
+                    markedPosition = Integer.parseInt(line.substring(7));
+                    lists[markedPosition-1] = "[ ] " + lists[markedPosition].substring(4);
+                    System.out.println("not done :(\n" + Integer.toString(markedPosition) + ". " + lists[markedPosition-1]);
+                } else {
+                    markedPosition = Integer.parseInt(line.substring(5));
+                    lists[markedPosition-1] = "[X] " + lists[markedPosition-1].substring(4);
+                    System.out.println("done :)\n" + Integer.toString(markedPosition) + ". " + lists[markedPosition-1]);
+                } //case where thrs no integer after mark/unmark in progress
+
+            } else { //main tracker
+                    lists[listsTracker] = "[ ] " + line;
                     listsTracker++;
                     System.out.println("added: " + line + "\n" + Jellyfish.space);
             }
