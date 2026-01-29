dd35dbe0f6204bc6dd53f28fbb38ac9cc7da6036
elliotjohnwu <elliotjohnwu@gmail.com>
2026-01-29 14:16:37 +0800

    Add to List in Echo class added

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Echo {
 
@@ -7,15 +8,30 @@ public class Echo {
         Scanner in = new Scanner(System.in);
 
         boolean isBye = false;
+        String[] lists = new String[100];
+        Integer listsTracker = 0;
 
         while (!isBye) {
-            System.out.print("Type something: ");
+            //System.out.print("Type something: ");
             line = in.nextLine();
             if (line.equalsIgnoreCase("bye")) {
                 isBye = true;
                 System.out.println(Jellyfish.space);
+
+            } else if (listsTracker>=99) {
+                isBye = true;
+                System.out.println(Jellyfish.space);
+                break;
+
+            } else if (line.equalsIgnoreCase("list")) {
+                for (int i = 0; i < listsTracker; i++ ) {
+                    System.out.println(Integer.toString(i+1) + ". " + lists[i]);
+                }
+                System.out.println("\n" + Jellyfish.space);
             } else {
-                System.out.println("You said: " + line+ "\n" + Jellyfish.space);
+                    lists[listsTracker] = line;
+                    listsTracker++;
+                    System.out.println("added: " + line + "\n" + Jellyfish.space);
             }
         }
     }
