bce2430b15dec243371221af8d47017365ca05f1
elliotjohnwu <elliotjohnwu@gmail.com>
2026-03-03 14:48:45 +0800

    Added DateParser for Event

--- src/main/java/jellyfish/task/Event.java
+++ src/main/java/jellyfish/task/Event.java
@@ -1,14 +1,33 @@
 package jellyfish.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
 
     protected String from;
     protected String to;
+    protected LocalDate fromDate;
+    protected LocalDate toDate;
 
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
+        
+        try {
+            this.fromDate = LocalDate.parse(from, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } catch (DateTimeParseException e) {
+            this.fromDate = null;
+        }
+        try {
+            this.toDate = LocalDate.parse(to, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } catch (DateTimeParseException e) {
+            this.toDate = null;
+        }
+        
+        
     }
     
     @Override
@@ -18,7 +37,21 @@ public class Event extends Task {
     
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        String formattedFrom;
+        if (fromDate != null) {
+            formattedFrom = fromDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        } else {
+            formattedFrom = from;
+        }
+        
+        String formattedTo;
+        if (toDate != null) {
+            formattedTo = toDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        } else {
+            formattedTo = to;
+        }
+        return "[E]" + super.toString() + " (from: " + formattedFrom + " to: " + formattedTo + ")";
     }
+    
 }
 
