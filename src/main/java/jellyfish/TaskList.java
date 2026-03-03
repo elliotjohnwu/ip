4e62a9ad5b684eaf075c02fb9e52cb6163e4244d
elliotjohnwu <elliotjohnwu@gmail.com>
2026-03-03 10:29:09 +0800

    Added TaskList to use function calls to access and edit ArrayList Task

---
+++ src/main/java/jellyfish/TaskList.java
@@ -0,0 +1,49 @@
+package jellyfish;
+
+import jellyfish.task.Task;
+import java.util.ArrayList;
+
+public class TaskList {
+	
+	private final ArrayList<Task> tasks;
+	
+	public TaskList() {
+		this.tasks = new ArrayList<>();
+	}
+	
+	public TaskList(ArrayList<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
+	public void addTask(Task task) {
+		tasks.add(task);
+	}
+	
+	public Task deleteTask(int index) {
+		return tasks.remove(index);
+	}
+	
+	public void markTask(int index) {
+		tasks.get(index).markDone();
+	}
+	
+	public void unmarkTask(int index) {
+		tasks.get(index).markNotDone();
+	}
+	
+	public Task getTask(int index) {
+		return tasks.get(index);
+	}
+	
+	public int size() {
+		return tasks.size();
+	}
+	
+	public boolean isEmpty() {
+		return tasks.isEmpty();
+	}
+	
+	public ArrayList<Task> getTasks() {
+		return tasks;
+	}
+}
