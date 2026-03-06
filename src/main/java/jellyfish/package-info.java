d9d84a401a188384e2ae4c1b88cab3286ca9c35b
elliotjohnwu <elliotjohnwu@gmail.com>
2026-03-06 11:30:43 +0800

    Added package-info file for JellyFish and JellyFish.task folders

---
+++ src/main/java/jellyfish/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains the main classes for the Jellyfish chatbot application,
+ * including the entry point, command parser, task list, storage, and UI.
+ */
+package jellyfish;
---
+++ src/main/java/jellyfish/task/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains the task classes used by the Jellyfish chatbot,
+ * including Todo, Deadline, and Event.
+ */
+package jellyfish.task;
